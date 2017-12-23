2e40d1854b8980381395305673b4617cc5fa002f
Bobby Jasuja <30449951+bjprogrammer@users.noreply.github.com>
2017-12-23 14:59:35 -0500

    Update config.php

--- nigerian traffic php/config.php
+++ nigerian traffic php/config.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_HOST", "fdb14.biz.nf");
-define("DB_USER", "2104393_bj");          
-define("DB_PASSWORD", "Bobby1993#");      
-define("DB_DATABASE", "2104393_bj");  
+define("DB_HOST", "f14.biz.nf");
+define("DB_USER", "");          
+define("DB_PASSWORD", "");      
+define("DB_DATABASE", "");  
 $base_url='http://www.bjprogrammer.co.nf/nigeriantraffic/';
 ?>
